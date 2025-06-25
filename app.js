51cafd2e9f06f4ce15551a5e454d97b5e8a4740c
hekate2 <howsoonisnow1121@gmail.com>
2025-06-25 12:47:24 -0700

    teehee

--- app.js
+++ app.js
@@ -5,51 +5,18 @@ import fetch from "node-fetch";
 
 const app = express();
 
-const FORBIDDEN_TAGS = [
-  "vulgar",
-  "obsolete"
-];
-
 let mots;
 
 app.get("/mot", async (req, res) => {
   try {
-    const infos = await choisirMot();
+    const infos = mots[Math.floor(Math.random() * mots.length)]
+
     res.json({ type: "success", infos });
   } catch (err) {
     console.error(err);
     res.status(500).json({ type: "error", message: err.message });
   }
 });
-
-async function choisirMot() {
-  let motChoisi;
-  let genre;
-
-  while (!genre) {
-    motChoisi = mots[Math.floor(Math.random() * mots.length)];
-
-    const headArgs = motChoisi?.head_templates?.[0]?.args || {};
-    const tags = motChoisi?.senses?.[0]?.tags || [];
-
-    if (["f", "m"].includes(headArgs["1"])) {
-      genre = headArgs["1"];
-    } else if (["f", "m"].includes(headArgs["g"])) {
-      genre = headArgs["g"];
-    } else if (tags.includes("feminine")) {
-      genre = "f";
-    } else if (tags.includes("masculine")) {
-      genre = "m";
-    }
-
-    // better
-    if (FORBIDDEN_TAGS.some(tag => tags.includes(tag))) {
-      genre = null;
-    }
-  }
-
-  return motChoisi
-}
 
 async function getWordData() {
   try {
@@ -60,6 +27,8 @@ async function getWordData() {
       throw new Error(await res.text());
     }
     mots = await res.json();
+
+    console.log(mots.length);
   } catch (err) {
     console.error("Failed to load mots.json:", err.message);
     process.exit(1);
